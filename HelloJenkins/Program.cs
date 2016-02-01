6da77127dbee1933b36ec9355346f30d5c054f4f
Michael <michacom@users.noreply.github.com>
2016-02-01 13:51:56 +0200

    Update Program.cs

--- HelloJenkins/Program.cs
+++ HelloJenkins/Program.cs
@@ -10,7 +10,7 @@ namespace HelloJenkins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
